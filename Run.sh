504517391aa541e1196dfd5a466ad2e680913971
LPMao <66683082+thekingofall@users.noreply.github.com>
2025-01-03 00:17:47 +0800

    Create Run.sh

---
+++ Run.sh
@@ -0,0 +1,4 @@
+```
+echo "alias k2p='bash /home/maolp/data5/All_zhuyue_inluolab/Software/Kraken2Polute/kraken2_processing.sh'" >> ~/.bashrc
+echo "alias k2py='python /home/maolp/data5/All_zhuyue_inluolab/Software/Kraken2Polute/Kraken2Polute_plots.py'" >> ~/.bashrc
+```
